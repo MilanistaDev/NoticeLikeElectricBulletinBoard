e40abcd7b1f5e0e92ac11bf631da8b01cbffce66
Takuya Aso <milanista224@gmail.com>
2020-03-16 18:04:44 +0900

    Implement UI and animation.

--- NoticeLikeElectricBulletinBoard/ViewController.swift
+++ NoticeLikeElectricBulletinBoard/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var announceLabel: UILabel!
+    @IBOutlet weak var leadingLayoutConstraint: NSLayoutConstraint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setUpUI()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setUpAnnounce()
+    }
 
+    private func setUpUI() {
+        self.navigationItem.title = "NoticeLikeElectricBulletinBoard"
+        self.leadingLayoutConstraint.constant = self.view.frame.width
+        self.announceLabel.text = "東西線は、強風の影響で１６日午前９時から東陽町－西船橋駅間の運転を見合わせ、中野－東陽町駅間での折り返し運転を実施しています。"
+    }
+
+    private func setUpAnnounce() {
+        let intrerval = self.announceLabel.frame.width > self.view.frame.width ?
+            10.0 * self.announceLabel.frame.width / self.view.frame.width : 10.0
+
+        self.leadingLayoutConstraint.constant -= self.announceLabel.frame.width * 2
+
+        UIView.animate(withDuration: TimeInterval(intrerval), delay: 0.0, options: [.repeat, .curveLinear], animations: {
+            self.announceLabel.frame.origin.x -= self.announceLabel.frame.width * 2
+        }, completion: nil)
+    }
 }
-
